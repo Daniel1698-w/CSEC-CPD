f2c2cc7075d57805738a7ecc7eb1b8d4b976ce8a
Daniel Gemechu <danielworkugemechu@gmail.com>
2026-04-19 17:43:50 +0300

    D_Same_Differences

---
+++ D_Same_Differences.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+t=int(input())
+
+for _ in range(t):
+    n=int(input())
+    count=0
+    di= defaultdict(int)
+    li=list(map(int,input().split()))
+
+    for i, val in enumerate(li):
+        if di[val-i]:
+            count+=di[val-i]
+        di[val-i]+=1
+
+    print(count)
+    
